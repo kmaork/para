c5b2720b51dee655e29a213d319ad3c5253c0650
Maor Kleinberger <kmaork@gmail.com>
2020-11-03 00:13:10 +0200

    moved test

--- tests/test.rs
+++ tests/test.rs
@@ -1,26 +1,21 @@
+use std::time::Instant;
 use para::scheduler::*;
 use para::consumer::*;
 use para::producer::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::time::Instant;
+#[test]
+fn test() {
+    let start = Instant::now();
 
-    #[test]
-    fn test() {
-        let start = Instant::now();
+    // Define pipeline
+    let print = |x| println!("{}", x);
+    let mult = (|x| x * 2).pipe(&print);
+    let mut prod = (1..10).pipe(&mult);
 
-        // Define pipeline
-        let print = |x| println!("{}", x);
-        let mult = (|x| x * 2).pipe(&print);
-        let mut prod = (1..10).pipe(&mult);
+    // Run pipeline
+    let s = Scheduler::new();
+    prod.add_to_scheduler(&s);
+    s.run(4);
 
-        // Run pipeline
-        let s = Scheduler::new();
-        prod.add_to_scheduler(&s);
-        s.run(4);
-        
-        println!("{:?}", Instant::now() - start);
-    }
+    println!("{:?}", Instant::now() - start);
 }
