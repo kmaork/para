212cf3a81ee646102dbe37dd4caf9aad2e4ce223
Maor Kleinberger <kmaork@gmail.com>
2020-11-03 00:12:05 +0200

    comment

--- tests/test.rs
+++ tests/test.rs
@@ -11,14 +11,16 @@ mod tests {
     fn test() {
         let start = Instant::now();
 
-        // define pipeline
+        // Define pipeline
         let print = |x| println!("{}", x);
         let mult = (|x| x * 2).pipe(&print);
         let mut prod = (1..10).pipe(&mult);
 
+        // Run pipeline
         let s = Scheduler::new();
         prod.add_to_scheduler(&s);
         s.run(4);
+        
         println!("{:?}", Instant::now() - start);
     }
 }
