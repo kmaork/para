4cf03c5755357263041ba9a61771552f69bf48f1
Maor Kleinberger <kmaork@gmail.com>
2021-01-16 23:39:47 +0200

    Improved bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,25 +2,25 @@
 
 use criterion::{criterion_group, criterion_main, Criterion};
 use para::*;
+use std::time::Duration;
 
-pub fn bench_one_map(c: &mut Criterion) {
-    c.bench_function("One map", |b| b.iter(|| {
+pub fn bench_pipeline(c: &mut Criterion) {
+    let mut group = c.benchmark_group("pipelines");
+    group.noise_threshold(0.03);
+    group.warm_up_time(Duration::from_secs(1));
+    group.measurement_time(Duration::from_secs(3));
+
+    group.bench_function("one_map", |b| b.iter(|| {
         run_pipeline!(0..10000 => |x| {x * 99;});
     }));
-}
-
-pub fn bench_two_maps(c: &mut Criterion) {
-    c.bench_function("Two maps", |b| b.iter(|| {
+    group.bench_function("two_maps", |b| b.iter(|| {
         run_pipeline!(0..10000 => |x| x * 99 => |x| {x - 1;});
     }));
-}
-
-pub fn bench_fanout(c: &mut Criterion) {
-    c.bench_function("Fanout", |b| b.iter(|| {
+    group.bench_function("fanout", |b| b.iter(|| {
         let f = Fanout::new(vec![&|x| { x + 1; }, &|x| { x - 1; }]);
         run_pipeline!(0..10000 => f);
     }));
 }
 
-criterion_group!(benches, bench_one_map, bench_two_maps, bench_fanout);
-criterion_main!(benches);
+criterion_group!(pipelines, bench_pipeline);
+criterion_main!(pipelines);
