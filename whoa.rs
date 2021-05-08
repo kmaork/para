037fefc738de45df8847492366a53b907a3ac779
Maor Kleinberger <kmaork@gmail.com>
2021-05-08 18:49:16 +0300

    Apparently push and then pop is less likely to cause than pop and then push

--- whoa.rs
+++ whoa.rs
@@ -1,38 +1,33 @@
 use crossbeam::deque::{Steal, Stealer, Worker};
 use crossbeam::thread;
+use std::time::Duration;
 
 fn main() {
     let w = Worker::<Box<u128>>::new_lifo();
     let s = w.stealer();
     let w2 = Worker::new_fifo();
-    let max = 200;
-    eprintln!("max {}", max);
+    let mut i = 0.;
     thread::scope(|sc| {
         sc.spawn(move |_| loop {
-            for x in 0..max {
-                for i in 0..x {
-                    for j in 0..x {
-                        for _ in 0..i {
-                            w.push(Box::new(1));
-                        }
-                        for _ in 0..j {
-                            if let Some(y) = w.pop() {
-                                if *y != 1 {
-                                    println!("{}, {}, {}", x, i, j);
-                                }
-                            }
-                        }
+            for _ in 0..60 {
+                w.push(Box::new(1));
+            }
+
+            for i in 0..40 {
+                if let Some(y) = w.pop() {
+                    if *y != 1 {
+                        println!("{}", i);
                     }
                 }
             }
+            for a in 0..1000 {
+                i *= a as f32;
+                i /= a as f32;
+            }
         });
         sc.spawn(move |_| loop {
-            for _ in 0..max {
+            for _ in 0..100 {
                 s.steal_batch(&w2);
-                for x in 0..5 {
-                    w2.push(Box::new(1));
-                    w2.pop();
-                }
             }
             while !w2.is_empty() {
                 if *w2.pop().unwrap() != 1 {
