ab68f5cb3d325b80497c83285cd3b74dd9174e7a
Maor Kleinberger <kmaork@gmail.com>
2021-05-08 11:54:49 +0300

    whoa still works 15sec

--- whoa.rs
+++ whoa.rs
@@ -3,23 +3,9 @@ use crossbeam::deque::{Steal, Stealer, Worker};
 use std::thread;
 use std::time::Duration;
 
-trait Cool: Sync + Send {
-    fn val(&self) -> u128;
-}
-impl Cool for u128 {
-    fn val(&self) -> u128 {
-        self.clone()
-    }
-}
-impl Cool for String {
-    fn val(&self) -> u128 {
-        self.len() as u128
-    }
-}
-
 fn main() {
     let t = 1;
-    let w = Worker::<Box<dyn Cool>>::new_lifo();
+    let w = Worker::<Box<u128>>::new_lifo();
     let ss: Vec<_> = (0..t).map(|_| w.stealer()).collect();
     crossbeam::thread::scope(|sc| {
         sc.spawn(move |_| loop {
@@ -28,11 +14,10 @@ fn main() {
                     for j in 0..x {
                         for _ in 0..i {
                             w.push(Box::new(1));
-                            w.push(Box::new("a".to_string()));
                         }
                         for _ in 0..j {
                             if let Some(y) = w.pop() {
-                                assert_eq!(y.val(), 1);
+                                assert_eq!(*y, 1);
                             }
                         }
                     }
@@ -43,13 +28,11 @@ fn main() {
             let w2 = Worker::new_lifo();
             sc.spawn(move |_| loop {
                 for i in 0..10 {
-                    for j in 0..10 {
-                        for _ in 0..i {
-                            s.steal_batch(&w2);
-                        }
-                        while !w2.is_empty() {
-                            assert_eq!(w2.pop().unwrap().val(), 1);
-                        }
+                    for _ in 0..i {
+                        s.steal_batch(&w2);
+                    }
+                    while !w2.is_empty() {
+                        assert_eq!(*w2.pop().unwrap(), 1);
                     }
                 }
             });
