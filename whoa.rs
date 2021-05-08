0a04361e5893c44d40aeb7e358d1a55d9680aaa9
Maor Kleinberger <kmaork@gmail.com>
2021-05-08 21:52:24 +0300

    simple fast reproduction

--- whoa.rs
+++ whoa.rs
@@ -6,23 +6,17 @@ fn main() {
     let w = Worker::<Box<u128>>::new_lifo();
     let s = w.stealer();
     let w2 = Worker::new_fifo();
-    let mut i = 0.;
     thread::scope(|sc| {
         sc.spawn(move |_| loop {
-            for _ in 0..60 {
+            for _ in 0..65 {
                 w.push(Box::new(1));
             }
-
             for i in 0..40 {
                 if let Some(y) = w.pop() {
                     if *y != 1 {
                         println!("{}", i);
                     }
                 }
-            }
-            for a in 0..1000 {
-                i *= a as f32;
-                i /= a as f32;
             }
         });
         sc.spawn(move |_| loop {
