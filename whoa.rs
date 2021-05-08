bea27c7bf3de15c72f04cce2cc877140afe37444
Maor Kleinberger <kmaork@gmail.com>
2021-05-08 17:45:27 +0300

    proved that pushes and pops are dependent on steal freq

--- whoa.rs
+++ whoa.rs
@@ -5,14 +5,15 @@ fn main() {
     let w = Worker::<Box<u128>>::new_lifo();
     let s = w.stealer();
     let w2 = Worker::new_fifo();
-    let mut v = (0..50000000).map(|_| Box::new(1));
+    let max = 200;
+    eprintln!("max {}", max);
     thread::scope(|sc| {
         sc.spawn(move |_| loop {
-            for x in 0..100 {
+            for x in 0..max {
                 for i in 0..x {
                     for j in 0..x {
                         for _ in 0..i {
-                            w.push(v.next().unwrap());
+                            w.push(Box::new(1));
                         }
                         for _ in 0..j {
                             if let Some(y) = w.pop() {
@@ -26,8 +27,12 @@ fn main() {
             }
         });
         sc.spawn(move |_| loop {
-            for _ in 0..100 {
+            for _ in 0..max {
                 s.steal_batch(&w2);
+                for x in 0..5 {
+                    w2.push(Box::new(1));
+                    w2.pop();
+                }
             }
             while !w2.is_empty() {
                 if *w2.pop().unwrap() != 1 {
--- yoy.py
+++ yoy.py
@@ -1,52 +1,33 @@
 import matplotlib.pyplot as plt
 import re
 import numpy as np
-from collections import defaultdict
-import matplotlib.ticker as mtick
 
+MAX = 200
 
 def rolling(x, w):
     return np.convolve(x, np.ones(w) / w, mode='valid')
 
 
-sep = lambda x: list(zip(*x))
-max_list = defaultdict(int)
-push_list = defaultdict(int)
-pop_list = defaultdict(int)
-arr = np.zeros((100, 100), dtype=int)
+max_list = []
+push_list = []
+pop_list = []
 
-for line in open('log', 'r'):
-    g = re.match(r'(?P<x>\d+), (?P<i>\d+), (?P<j>\d+)', line)
-    max_sample = int(g['x'])
-    push_sample = int(g['i'])
-    pop_sample = int(g['j'])
-    max_list[max_sample] += 1
-    push_list[push_sample] += 1
-    pop_list[pop_sample] += 1
-    arr[push_sample][pop_sample] += 1
-
-
-def graph():
-    for d in [max_list, push_list, pop_list]:
-        w = 5
-        x, y = sep(sorted(d.items()))
-        # plt.plot(np.fft.fft(y))
-        if w:
-            y = [val * 100 / sum(y) for val in rolling(y, w)]
-            x = x[w // 2:-w // 2 + 1]
-        plt.plot(x, y)
-
-    plt.legend(["Max", "Pushes", "Pops"])
-    plt.xlabel("Num")
-    plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter())
-    plt.show()
+for f in ['log200', 'log200.2', 'log200.3', 'log200.4']:
+    for line in open(f, 'r'):
+        g = re.match(r'(?P<max>\d+), (?P<push>\d+), (?P<pop>\d+)', line)
+        max_list.append(int(g['max']))
+        push_list.append(int(g['push']))
+        pop_list.append(int(g['pop']))
 
 
 def heatmap():
-    plt.imshow(arr, cmap='hot', interpolation='nearest', origin='lower', vmax=arr.max())
+    arr = np.zeros((MAX, MAX), dtype=int)
+    for push, pop in zip(push_list, pop_list):
+        arr[push][pop] += 1
+    plt.imshow(arr, cmap='hot', origin='lower')
     plt.xlabel("Pushes")
     plt.ylabel("Pops")
     plt.show()
 
-
+print(np.mean(push_list), np.mean(pop_list))
 heatmap()
