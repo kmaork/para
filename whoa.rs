7c6cd3cd3b294ff194fb7e0ef9e368f8a6bf5b37
Maor Kleinberger <kmaork@gmail.com>
2021-05-08 22:45:39 +0300

    simplest recreation

--- whoa.rs
+++ whoa.rs
@@ -3,31 +3,20 @@ use crossbeam::thread;
 use std::time::Duration;
 
 fn main() {
-    let w = Worker::<Box<u128>>::new_lifo();
+    let w = Worker::new_lifo();
     let s = w.stealer();
-    let w2 = Worker::new_fifo();
     thread::scope(|sc| {
         sc.spawn(move |_| loop {
             for _ in 0..65 {
                 w.push(Box::new(1));
             }
-            for i in 0..40 {
-                if let Some(y) = w.pop() {
-                    if *y != 1 {
-                        println!("{}", i);
-                    }
-                }
+            for _ in 0..40 {
+                w.pop();
             }
         });
         sc.spawn(move |_| loop {
-            for _ in 0..100 {
-                s.steal_batch(&w2);
-            }
-            while !w2.is_empty() {
-                if *w2.pop().unwrap() != 1 {
-                    // println!("bad");
-                }
-            }
+            let w2 = Worker::new_fifo();
+            s.steal_batch(&w2);
         });
     })
     .unwrap();
