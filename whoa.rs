0b6886a4dfb1cf7637855417b65718613a783ea5
Maor Kleinberger <kmaork@gmail.com>
2021-05-08 13:36:36 +0300

    still fails with preallocated vector

--- whoa.rs
+++ whoa.rs
@@ -1,19 +1,18 @@
-use crossbeam;
 use crossbeam::deque::{Steal, Stealer, Worker};
-use std::thread;
-use std::time::Duration;
+use crossbeam::thread;
 
 fn main() {
-    let t = 1;
     let w = Worker::<Box<u128>>::new_lifo();
-    let ss: Vec<_> = (0..t).map(|_| w.stealer()).collect();
-    crossbeam::thread::scope(|sc| {
+    let s = w.stealer();
+    let w2 = Worker::new_fifo();
+    let mut v = (0..10000000).map(|_| Box::new(1));
+    thread::scope(|sc| {
         sc.spawn(move |_| loop {
             for x in 0..1000 {
                 for i in 0..x {
                     for j in 0..x {
                         for _ in 0..i {
-                            w.push(Box::new(1));
+                            w.push(v.next().unwrap());
                         }
                         for _ in 0..j {
                             if let Some(y) = w.pop() {
@@ -24,19 +23,14 @@ fn main() {
                 }
             }
         });
-        for s in ss {
-            let w2 = Worker::new_lifo();
-            sc.spawn(move |_| loop {
-                for i in 0..1000 {
-                    for _ in 0..i {
-                        s.steal_batch(&w2);
-                    }
-                    while !w2.is_empty() {
-                        assert_eq!(*w2.pop().unwrap(), 1);
-                    }
-                }
-            });
-        }
+        sc.spawn(move |_| loop {
+            for _ in 0..100 {
+                s.steal_batch(&w2);
+            }
+            while !w2.is_empty() {
+                assert_eq!(*w2.pop().unwrap(), 1);
+            }
+        });
     })
     .unwrap();
 }
