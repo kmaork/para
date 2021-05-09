b82f8e4de9884d2185c9290b4e76e6f8efb238f8
Maor Kleinberger <kmaork@gmail.com>
2021-05-09 21:39:53 +0300

    we pop the shit twice

--- whoa.rs
+++ whoa.rs
@@ -1,59 +1,39 @@
 use crossbeam::deque::{Steal, Stealer, Worker};
 use crossbeam::thread;
-use std::borrow::BorrowMut;
-use std::cell::RefCell;
 use std::collections::HashSet;
-use std::ops::{Deref, DerefMut};
 use std::sync::{Arc, Mutex};
-use std::time::Duration;
-
-thread_local!(static FOO: RefCell<HashSet<u64>> = RefCell::new(HashSet::new()));
-struct Yay {
-    i: u64,
-    m: Arc<Mutex<HashSet<u64>>>,
-}
-impl Yay {
-    fn new(i: u64, m: Arc<Mutex<HashSet<u64>>>) -> Self {
-        Self { i, m }
-    }
-}
-impl Drop for Yay {
-    fn drop(&mut self) {
-        FOO.with(|f| {
-            assert!(f.borrow_mut().insert(self.i));
-        });
-        if !self.m.lock().unwrap().insert(self.i) {
-            println!("{}", self.i);
-        }
-    }
-}
-impl Deref for Yay {
-    type Target = u64;
-
-    fn deref(&self) -> &Self::Target {
-        &self.i
-    }
-}
 
 fn main() {
     let w = Worker::new_lifo();
     let s = w.stealer();
     let mut i = 0;
+    let h = Arc::new(Mutex::new(HashSet::new()));
+    let h2 = h.clone();
     thread::scope(|sc| {
-        let m = Arc::new(Mutex::new(HashSet::new()));
         sc.spawn(move |_| loop {
             for _ in 0..65 {
-                w.push(Yay::new(i, m.clone()));
+                w.push(i);
                 i += 1;
             }
             for _ in 0..40 {
-                w.pop();
+                if let Some(p) = w.pop() {
+                    if !h.lock().unwrap().insert(p) {
+                        println!("yo {}", p);
+                    }
+                }
             }
         });
+        let w2 = Worker::new_fifo();
         sc.spawn(move |_| loop {
-            let w2 = Worker::new_fifo();
             for _ in 0..100 {
                 s.steal_batch(&w2);
+            }
+            while !w2.is_empty() {
+                if let Some(p) = w2.pop() {
+                    if !h2.lock().unwrap().insert(p) {
+                        println!("ya {}", p);
+                    }
+                }
             }
         });
     })
